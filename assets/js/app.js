532b6de610b5ea578fa310d87774427f338cdcfe
Vapak <46297246+Vapak@users.noreply.github.com>
2020-10-05 23:52:47 +0700

    fixes + animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
-$('.testimonial__carousel').slick();
+//Smooth scroll
+$("[data-scroll]").on("click", function(event){
+  event.preventDefault();
+  var $this = $(this),
+      blockId = $this.data('scroll'),
+      blockOffset = $(blockId).offset().top;
+  // $("#nav a").removeClass("active");
+  // $this.addClass("active");
+
+  $("html , body").animate({
+    scrollTop:blockOffset
+  }, 500);
+});
