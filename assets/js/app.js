77f5c7d1ffe7a11f6f22e64c63ce3a08f4f89bc9
Vapak <46297246+Vapak@users.noreply.github.com>
2020-10-08 01:18:03 +0700

    adaptive + carousel + scroll buttons + mobile menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,49 @@ $("[data-scroll]").on("click", function(event){
     scrollTop:blockOffset
   }, 500);
 });
+
+
+//Slick Carousel
+$(".testimonial__carousel").slick({
+  arrows:false,
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: false,
+  variableWidth: true
+});
+
+//Pricing period
+$(".pricing__button").on("click", function(event){
+  $(".pricing__button").removeClass("pricing__button--active");
+  $(this).addClass("pricing__button--active");
+});
+
+//Burger Menu
+$(".nav-burger").on("click", function(event){
+  $(this).toggleClass("active")
+  $(".header__navigation").toggleClass("header__navigation--burger");
+  if($(this).hasClass("active")){
+      $(".header__navigation").css("display", "flex");}
+      else{
+        $(".header__navigation").css("display", "none");
+      }
+
+});
+
+//Scroll Down
+$(window).on("scroll", function(){
+  if($(this).scrollTop()>=$(window).innerHeight()) {
+    $(".scroll__down").removeClass("scroll__down--active");
+    $(".scroll__up").addClass("scroll__up--active");
+  }
+  else{
+    $(".scroll__down").addClass("scroll__down--active");
+    $(".scroll__up").removeClass("scroll__up--active");
+}
+});
+
+$(".scroll__down").on("click", function(event){
+  $(this).removeClass("scroll__down--active");
+});
